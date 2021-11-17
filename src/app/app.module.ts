10c73a53378f9c74d7c477a06b84971a7b1762a8
Cerkes Mirza <almas1610@gmail.com>
2021-11-17 07:37:36 +0600

    *ng-todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatSliderModule
   ],
   imports: [
     BrowserModule,
